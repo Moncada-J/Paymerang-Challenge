3f14a87d9f04624015df44a20c867e7701a7bb05
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-06-19 15:46:04 -0700

    adds payee object data (except for address)

--- src/components/RenderAddress/RenderAddress.jsx
+++ src/components/RenderAddress/RenderAddress.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import statements from '../../sample.json';
 export default function RenderAddress() {
-      const address = statements.map((a, idx) => (
-        <RenderAddress address={a} key={idx} index={idx} />
-    ));
     return (
             <div>
                     <h4>
-                        Address: {address}
+                        Address: 
                     </h4>
             </div>
     )
--- src/components/StatementCard/StatementCard.jsx
+++ src/components/StatementCard/StatementCard.jsx
@@ -7,8 +7,9 @@ export default function StatementCard ({statement}) {
              <h4>Payee: {statement.Payee.Name}</h4>
             <span> <strong>F:</strong> {statement.Payee.Fax} /  <strong>P:
             </strong> {statement.Payee.Phone}</span>
-            {/* <span><strong>Address</strong>{statement.Payee.Address}</span> */}
-            <RenderAddress address={statement.Payee.Address}/>
+            <RenderAddress addressList={statement.Payee.Address}/>
+            <h5><strong>Attention:</strong> {statement.Payee.Attention} <br />
+            Submitted: {statement.Payee.SubmissionDate}</h5>
         </div>
-    )
+    );
 }
