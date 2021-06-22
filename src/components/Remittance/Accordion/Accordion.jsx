0d1abae44d74a3a78820a235eb679839857b8c29
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-06-21 21:56:30 -0700

    alters remittance design to table, fixes page margin

--- src/components/Remittance/Accordion/Accordion.jsx
+++ src/components/Remittance/Accordion/Accordion.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
+// import React from 'react';
 
-export default function Accordion ({r}) {
-return (
-    <div class="accordion" id="accordionExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-                <button
-                    class="accordion-button"
-                    type="button"
-                    data-mdb-toggle="collapse"
-                    data-mdb-target="#collapseOne"
-                    aria-expanded="true"
-                    aria-controls="collapseOne"
-                    >
-                    Remittance Details
-                </button>
-            </h2>
-        <div
-        id="collapseOne"
-        class="accordion-collapse collapse show"
-        aria-labelledby="headingOne"
-        data-mdb-parent="#accordionExample"
-        >
-            <div class="accordion-body">
-                Payor: {r.PayorName}
-                    <br />
-                Payor #: {r.PayorId}
-                    <br />
-                Invoice #: {r.InvoiceNo}
-                    <br />
-                Details: {r.Description}
-                    <br />
-                Amount (USD): {r.Amount}
-            </div>
-        </div>
-    </div>
-   </div>
-  );
-}
+// export default function Accordion ({r}) {
+// return (
+// //     <div class="accordion" id="accordionExample">
+// //         <div class="accordion-item">
+// //             <h2 class="accordion-header" id="headingOne">
+// //                 <button
+// //                     class="accordion-button"
+// //                     type="button"
+// //                     data-mdb-toggle="collapse"
+// //                     data-mdb-target="#collapseOne"
+// //                     aria-expanded="true"
+// //                     aria-controls="collapseOne"
+// //                     >
+// //                     View <strong className="accord-str"> {r.PayorName} </strong> Report
+// //                 </button>
+// //             </h2>
+// //         <div
+// //         id="collapseOne"
+// //         class="accordion-collapse collapse show"
+// //         aria-labelledby="headingOne"
+// //         data-mdb-parent="#accordionExample"
+// //         >
+// //             <div class="accordion-body">
+// //                 Payor: {r.PayorName}
+// //                     <br />
+// //                 Payor #: {r.PayorId}
+// //                     <br />
+// //                 Invoice #: {r.InvoiceNo}
+// //                     <br />
+// //                 Details: {r.Description}
+// //                     <br />
+// //                 Amount (USD): {r.Amount}
+// //              </div>
+// //         </div>
+// //     </div>
+// //    </div>
+//   );
+// }
--- src/components/Remittance/RenderRemittance/RenderRemittance.jsx
+++ src/components/Remittance/RenderRemittance/RenderRemittance.jsx
@@ -2,14 +2,38 @@ import React from 'react';
 import Accordion from '../Accordion/Accordion';
 
 export default function RenderRemittance({remittance}) {
+    
     return (
         // this component maps out the 
         // array nested inside the json data
         // for remittance keys
-        <div>
-            {remittance.map((r) => (
-                <Accordion r={r}/>
-            ))}
-        </div>
+    <div>
+
+        <table class="table data-table">
+            <thead className="table-header">
+                <tr>
+                <th scope="col">Payor Name</th>
+                <th scope="col">Payor ID</th>
+                <th scope="col">Invoice No.</th>
+                <th scope="col">Amount (USD)</th>
+                <th scope="col">Transaction <br /> Details</th>
+                </tr>
+            </thead>
+        {remittance.map((remittance, index) => (
+            <tbody style={{
+        backgroundColor: index % 2 ?  "#dcdfe2" : "#ffffff", color: index % 2 ? "#426d84" : "#575457"
+      }}>
+                <tr>
+                <th scope="row">{remittance.PayorName}</th>
+                <td>{remittance.PayorId}</td>
+                <td>{remittance.InvoiceNo}</td>
+                <td>{remittance.Amount}</td>
+                <td colspan="2">{remittance.Description}</td>
+                </tr>
+            </tbody>
+        ))}
+        </table>
+      
+</div>
     )
 }
