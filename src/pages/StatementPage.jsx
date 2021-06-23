179963c55309bf7c928d855316c245662216790b
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-06-22 20:27:03 -0700

    Updates

--- src/pages/StatementPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RenderStatements from '../components/RenderStatements/RenderStatements';
-import './StatementPage.css';
-
- export default function StatementPage() {
-    return (
-          <RenderStatements />
-    )
-}
